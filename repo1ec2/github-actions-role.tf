3ebac194d60d8cabffef066a575824b364aebad6
lattey0 <adhikariasutosh999@gmail.com>
2025-07-15 09:24:19 +0000

    with codes

--- repo1ec2/github-actions-role.tf
+++ repo1ec2/github-actions-role.tf
@@ -2,21 +2,23 @@ resource "aws_iam_role" "github_actions_oidc" {
   name = "GitHubActionsRole"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = "arn:aws:iam::702865854817:oidc-provider/token.actions.githubusercontent.com"
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub": "repo:asutosh/repo-1-ec2:ref:refs/heads/main"
-          }
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:aws:iam::702865854817:oidc-provider/token.actions.githubusercontent.com"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+                },
+                "StringLike": {
+                    "token.actions.githubusercontent.com:sub": "repo:YOUR_GITHUB_ORG/YOUR_REPO_NAME:ref:refs/heads/main"
+                }
+            }
         }
-      }
     ]
   })
 
