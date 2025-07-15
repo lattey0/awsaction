c204fd90fcb0a3df2e65f8f66cc56db2dbb7884a
lattey0 <adhikariasutosh999@gmail.com>
2025-07-15 08:31:00 +0000

    with codes

---
+++ repo1ec2/github-actions-role.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_role" "github_actions_oidc" {
+  name = "GitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::<your-account-id>:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:<your-org>/<your-repo>:*"
+          },
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Creator = "asutosh"
+  }
+}
+
+# Attach AdministratorAccess (for full control - restrict later)
+resource "aws_iam_role_policy_attachment" "github_actions_full_access" {
+  role       = aws_iam_role.github_actions_oidc.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
