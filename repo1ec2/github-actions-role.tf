675ea80ce731cd8fe3a0882852cf45cb3685738c
lattey0 <adhikariasutosh999@gmail.com>
2025-07-15 09:31:50 +0000

    with codes

--- repo1ec2/github-actions-role.tf
+++ repo1ec2/github-actions-role.tf
@@ -2,23 +2,24 @@ resource "aws_iam_role" "github_actions_oidc" {
   name = "GitHubActionsRole"
 
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Federated": "arn:aws:iam::702865854817:oidc-provider/token.actions.githubusercontent.com"
-            },
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Condition": {
-                "StringEquals": {
-                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-                },
-                "StringLike": {
-                    "token.actions.githubusercontent.com:sub": "repo:YOUR_GITHUB_ORG/YOUR_REPO_NAME:ref:refs/heads/main"
-                }
-            }
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::702865854817:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Action    = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          },
+          # CORRECTED: Allow any branch, pull request, or tag from your specific repository
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:asutosh-g/your-repo-name:*"
+          }
         }
+      }
     ]
   })
 
@@ -26,5 +27,3 @@ resource "aws_iam_role" "github_actions_oidc" {
     Creator = "asutosh"
   }
 }
-
-
